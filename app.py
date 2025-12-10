1fe3b4e50e8e52d32cbded49ea4f1e9c36047f59
ckumar <ckumar@bn.com>
2025-12-11 02:35:51 +0530

    app is ready

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+        <html>
+        <body>
+            <form action="/greet" method="POST">
+                Enter your name: <input type="text" name="username">
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+        </html>
+    '''
+
+@app.route('/greet', methods=['POST'])
+def greet():
+    user_input = request.form['username']
+    return f"Hello {user_input}, Welcome to this app for Docker demonstration. Please consider like and subscribe to the channel."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
+'''
 import streamlit as st
 
 # Streamlit UI
@@ -16,3 +42,4 @@ fifth_power = n ** 5
 st.write(f"The square of {n} is: {square}")
 st.write(f"The cube of {n} is: {cube}")
 st.write(f"The fifth power of {n} is: {fifth_power}")
+'''
